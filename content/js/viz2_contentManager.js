f17bedb805f8370ba8c9b65d44cd8a94a9c1117a
Jen Paek <jpaeker7@gmail.com>
2013-12-02 16:02:08 -0500

    viz2 fixed bug with updating selected, updated header text & keys by filter type

--- content/js/viz2_contentManager.js
+++ content/js/viz2_contentManager.js
@@ -1,6 +1,6 @@
   function viz2_contentManager() {
-	  highlighted_nodes = [];
-	  highlighted_nodes_position = {};
+	  viz2_state = "filterby_popularity";
+	  
 	  
 	  $('#ui-id-2').click(function(e) { 
 	  	if (default_view == true) {
@@ -9,12 +9,28 @@
 		    popularity_exists = true;
 			default_view = false;
 			$("#dropdownText").text("Popularity");
+			$("#viz2_header").html("Win | Lose Rate ordered by the most played heroes for The International 3 Tournament.<br>Ordered by clusters of 5 from most played to least played.");
+			$("#viz2_header_images").html("<img src='content/images/viz2_popularityKey.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/><img src='content/images/viz2_selectKey.png' style='margin-left:30px;'/>");
+			
 		}
-		if (filterbystats_view == true) {
+		/*if (filterbystats_view == true) {
 			$('#viz2_content').remove();
 			popularity_exists = false;
 			viz2_byTypeWrapper();
 			viz2_content_bySelected();
+		}*/
+		
+		if (current_state != movement) {
+			popularity_exists = false;
+			default_view == false;
+			$('#'+viz2_state).click();
+			current_state = movement;	
+		}
+		
+		if ((current_table_movement != table_movement) && (viz2_state == "filterby_heroStatistics")) {
+			$('#'+viz2_state).click();
+			current_table_movement = table_movement;
+			
 		}
 		  
 	  });
@@ -26,8 +42,10 @@
 			viz2_content_bySelected();
 			filterbystats_view = true;
 			d3.select('#fragment-2').attr('style', 'height: 1800px');
-			$("#dropdownText").text("Selected Heor Statistics");
-			
+			$("#dropdownText").text("Selected Hero Statistics");
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_heroStatistics";
 		});
 	  
 	  $('#filterby_popularity').click(function(e) { 
@@ -39,7 +57,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Popularity");
-			
+			$("#viz2_header").html("Win | Lose Rate ordered by the most played heroes for The International 3 Tournament.<br>Ordered by clusters of 5 from most played to least played.");
+			$("#viz2_header_images").html("<img src='content/images/viz2_popularityKey.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/><img src='content/images/viz2_selectKey.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_popularity";
 			
 		});
 		
@@ -52,7 +72,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Type - Carry");
-			
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table by Hero Type");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_type_carry";
 		});
 	$('#filterby_type_mid').click(function(e) { 
 			$('#viz2_content').remove();
@@ -63,6 +85,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Type - Mid");
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table by Hero Type");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_type_mid";
 		});
 	$('#filterby_type_offlane').click(function(e) { 
 			$('#viz2_content').remove();
@@ -73,6 +98,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Type - Offlane");
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table by Hero Type");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_type_offlane";
 		});
 	$('#filterby_type_jungler').click(function(e) { 
 			$('#viz2_content').remove();
@@ -83,6 +111,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Type - Farming Support");
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table by Hero Type");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_type_jungler";
 		});
 	$('#filterby_type_support').click(function(e) { 
 			$('#viz2_content').remove();
@@ -93,6 +124,9 @@
 			filterbystats_view = false;
 			$("#viz2_content_wrapper").addClass("center_horizontal");
 			$("#dropdownText").text("Type - Hard Support");
+			$("#viz2_header").text("Win | Lose Rate shown and selected in the Hero Statistics table by Hero Type");
+			$("#viz2_header_images").html("<img src='content/images/viz2_key.png' /><img src='content/images/viz2_hoverInstructions.png' style='margin-left:30px;'/>");
+			viz2_state = "filterby_type_support";
 		});
 		
   }
